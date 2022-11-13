23ed0575128b1d6707ddd45bd8c9e46e9c4fe118
CuongHo <cuong.holeduy@gmail.com>
2022-11-13 21:06:39 +0700

    complete app

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -8,12 +8,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries: [String] = []
+    var score = 0
+    var correctAnswer = 0
+    var numberOfQuestion = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
 
-
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        numberOfQuestion += 1
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = "\(countries[correctAnswer].uppercased()) - Score: \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong. That flag is \(countries[sender.tag].uppercased())"
+            if score > 0 {
+                score -= 1
+            }
+        }
+        
+        if numberOfQuestion == 10 {
+            let ac = UIAlertController(title: "Victory!!!", message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Reset", style: .destructive, handler: resetGame))
+            
+            present(ac, animated: true)
+            
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            ac.addAction(UIAlertAction(title: "Reset", style: .destructive, handler: resetGame))
+            
+            present(ac, animated: true)
+        }
+    }
+    
+    func resetGame(action: UIAlertAction) {
+        score = 0
+        numberOfQuestion = 0
+        askQuestion()
+    }
 }
 
